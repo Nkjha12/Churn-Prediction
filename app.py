a2dee9e46bbafda2ff904465f2581eb082583f2a
Nkjha12 <nitinjha@Nitins-MacBook-Air.local>
2024-11-06 21:34:11 +0530

    Rest API files added

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 import pandas as pd
-from fastapi import FastAPI
-import numpy as np
-from sklearn.preprocessing import StandardScaler
+from fastapi import FastAPI, Request ,  File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 import joblib
-
-model = joblib.load('model.pkl')
+import io
 
 app = FastAPI()
+model = joblib.load('model.pkl')
+
 
 x_test = pd.read_csv("x_test.csv")
 
-x_test = x_test.iloc[:, :10]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
 
-@app.get("/predict_all")
-def predict_all():
-    # Prepare input data from x_test
-    predictions = model.predict(x_test)
-    return {"predictions": predictions.tolist()}
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    with open("index.html") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
 
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    
+    contents = await file.read()
+    data = pd.read_csv(io.StringIO(contents.decode("utf-8")))
+    
+    
+    features_for_prediction = data.iloc[0, :10] 
+    features_for_prediction = features_for_prediction.values.reshape(1, -1)
+   
+    prediction = model.predict(features_for_prediction)
+    
+    return {"prediction": int(prediction[0])}
